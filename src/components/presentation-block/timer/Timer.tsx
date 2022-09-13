510cc45cf8668f50e4375bc81dbf646c0df3ce6f
margarita <margaritakh0708@gmail.com>
2022-09-13 18:15:39 +0300

    add timer feature

--- src/components/presentation-block/timer/Timer.tsx
+++ src/components/presentation-block/timer/Timer.tsx
@@ -1,33 +1,32 @@
 
 import { useState, useEffect } from 'react';
 
-export const Timer: React.FC = () => {
 
+export const Timer: React.FC = () => {
 
 
     const [timeDistance, setTimeDistance] = useState<number>(new Date("Jan 14, 1970 15:37:25").getTime());
 
-
     useEffect(() => {
         const timer = setInterval(() => {
-         setTimeDistance(() => timeDistance >= 1 ? timeDistance - 1 : 0);
+            setTimeDistance((c: number) => c >= 1000 ? c - 1000 : 0); //форму функционального обновления хука setState, которая позволяет указать, как должно меняться состояние, не ссылаясь явно на текущее состояние
             console.log('this', timeDistance)
         }, 1000)
 
         return () => clearInterval(timer)
-    }, [timeDistance])
+    }, [])
 
- 
-    let days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
+
+    let seconds = Math.floor((timeDistance / 1000) % 60);
+    let minutes = Math.floor((timeDistance / 1000 / 60) % 60);
+    let hours = Math.floor((timeDistance / 1000 / 60 / 60) % 24);
+    let days = Math.floor(timeDistance / 1000 / 60 / 60 / 24);
 
     return (
         <div className='presentation-block__timer'>
-            <div className='presentation-block__timer-item'>{days} <span className='prentation-block__timer-title'>Дней</span></div>
-            <div className='presentation-block__timer-item'>{hours}<span className='prentation-block__timer-title'>Часов</span></div>
-            <div className='presentation-block__timer-item'>{minutes}<span className='prentation-block__timer-title'>Минут</span></div>
-            <div className='presentation-block__timer-item'>{seconds}<span className='prentation-block__timer-title'>Секунд</span></div>
+            <div className='presentation-block__timer-item'><span className='presentation-block__timer-date'>{days >= 10 ? days : `0${days}`}</span> <span className='presentation-block__timer-title'>Дней</span></div>
+            <div className='presentation-block__timer-item'><span className='presentation-block__timer-date'>{hours >= 10 ? hours : `0${hours}`}</span><span className='presentation-block__timer-title'>Часов</span></div>
+            <div className='presentation-block__timer-item'><span className='presentation-block__timer-date'>{minutes >= 10 ? minutes : `0${minutes}`}</span><span className='presentation-block__timer-title'>Минут</span></div>
+            <div className='presentation-block__timer-item'><span className='presentation-block__timer-date'>{seconds >= 10 ? seconds : `0${seconds}`}</span><span className='presentation-block__timer-title'>Секунд</span></div>
         </div>)
 }
