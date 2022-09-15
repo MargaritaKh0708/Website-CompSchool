779dbd09cbdc1ef863a7224fea50ad649632763c
margarita <margaritakh0708@gmail.com>
2022-09-15 14:35:36 +0300

    start of upgrating styles with media-changes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,58 @@
-import logo from 'assets/img/logo.svg'
+import logo from "assets/img/logo.svg";
+import { useState } from "react";
 
 export const Header: React.FC = () => {
-    return (
-        <header className='header container'>
-            <div className='header__wrapper'>
-                <div className='header__logo'><img src={logo} alt="logo" /></div>
-                <nav className='navigation'>
-                <ul className='navigation__list'>
-                    <li className='navigation__item'><a className='navigation__link' href="#main">Главная</a></li>
-                    <li className='navigation__item'><a className='navigation__link' href="#couses">Курсы</a></li>
-                    <li className='navigation__item'><a className='navigation__link' href="#schedule">Расписание</a></li>
-                    <li className='navigation__item'><a className='navigation__link' href="#teachers">Преподаватели</a></li>
-                    <li className='navigation__item'><a className='navigation__link' href="#mailing">Рассылка</a></li>
-                    <li className='navigation__item'><a className='navigation__link' href="#contacts">Контакты</a></li>
-                </ul>
-                <button className='navigation__btn'>
-                    Зайти в кабинет
-                </button>
-            </nav>
+  const [burgerActive, setBurgerActive] = useState<boolean>(true);
+
+  return (
+    <header className="header container">
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <nav className="navigation">
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <a className="navigation__link" href="#main">
+                Главная
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#couses">
+                Курсы
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#schedule">
+                Расписание
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#teachers">
+                Преподаватели
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#mailing">
+                Рассылка
+              </a>
+            </li>
+            <li className="navigation__item">
+              <a className="navigation__link" href="#contacts">
+                Контакты
+              </a>
+            </li>
+          </ul>
+          <button className="navigation__btn">Зайти в кабинет</button>
+          <button name="burger" type="button" className="header__btn">
+            <div className="burger" onClick={() => setBurgerActive(true)}>
+              <span className="burger__item"></span>
+              <span className="burger__item"></span>
+              <span className="burger__item"></span>
             </div>
-        </header>
-    )
-}
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
+};
